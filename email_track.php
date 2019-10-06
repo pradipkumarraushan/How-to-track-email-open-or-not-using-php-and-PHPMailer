dc8ccdaaf5780295c0cf91f50cc6e03ffd75190a
PRADIP KUMAR RAUSHAN <44359568+pradipkumarraushan@users.noreply.github.com>
2019-10-06 18:43:43 +0530

    Update email_track.php

--- email_track.php
+++ email_track.php
@@ -8,10 +8,9 @@ if(isset($_GET["code"]))
 {
 
 	$query = "
-	INSERT INTO  email_track (email_track_code,email_status,email_open_datetime) 
-	VALUES ('".$_GET["code"]."','yes','".date("Y-m-d H:i:s", STRTOTIME(date('h:i:sa')))."') ";
+	INSERT INTO  email_track (email_track_code,email_status,email_open_datetime) VALUES (?,?,?) ";
 	$statement = $connect->prepare($query);
-	$statement->execute();
+	$statement->execute([''.$_GET["code"].'','yes',''.date("Y-m-d H:i:s", STRTOTIME(date('h:i:sa'))).'']);
 
 }
 
